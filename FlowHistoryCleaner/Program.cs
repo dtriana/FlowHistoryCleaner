05eb657a1627e4d965f443ca657e1ac721c1a59f
David Triana <dtriana@twoconnect.com>
2024-10-30 10:42:16 -0500

    Update to dotnet 8

--- FlowHistoryCleaner/Program.cs
+++ FlowHistoryCleaner/Program.cs
@@ -1,69 +1,61 @@
-﻿using System;
-using Azure;
+﻿using Azure;
 using Azure.Data.Tables;
 using Azure.Data.Tables.Models;
 using Azure.Storage.Queues;
 using Azure.Storage.Queues.Models;
 
-namespace FlowHistoryCleaner
+var connStr = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+if(string.IsNullOrEmpty(connStr))
+    connStr= "UseDevelopmentStorage=true";
+Console.WriteLine($"will use the connection string {connStr} ");
+if (!PromptConfirmation("Are you sure you want to clean all Tables and Queues ?"))
 {
-    class Program
+    return;
+}
+
+var TserviceClient = new TableServiceClient(connStr);
+
+Pageable<TableItem> queryTableResults = TserviceClient.Query();
+foreach (var table in queryTableResults)
+{
+    if (table.Name.StartsWith("flow"))
     {
-        static void Main(string[] args)
+        TserviceClient.DeleteTable(table.Name);
+        Console.WriteLine($"-Table {table.Name} is Deleted ");
+    }
+
+}
+
+
+var QserviceClient = new QueueServiceClient(connStr);
+
+//list all queues in the storage account
+var myqueues = QserviceClient.GetQueues().AsPages();
+
+//then you can write code to list all the queue names          
+foreach (Azure.Page<QueueItem> queuePage in myqueues)
+{
+    foreach (var queue in queuePage.Values)
+    {
+        if (queue.Name.StartsWith("flow"))
         {
-
-            string conn_str = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-            Console.WriteLine($"will use the connection string {conn_str} ");
-            if (!PromptConfirmation("Are you sure you want to clean all  Tables and Queues ?"))
-            {
-                return;
-            }
-
-            var TserviceClient = new TableServiceClient(conn_str);
-
-            Pageable<TableItem> queryTableResults = TserviceClient.Query();
-            foreach (TableItem table in queryTableResults)
-            {
-                if (table.Name.StartsWith("flow"))
-                {
-                    TserviceClient.DeleteTable(table.Name);
-                    Console.WriteLine($"-Table {table.Name} is Deleted ");
-                }
-
-            }
-
-
-            QueueServiceClient QserviceClient = new QueueServiceClient(conn_str);
-
-            //list all queues in the storage account
-            var myqueues = QserviceClient.GetQueues().AsPages();
-
-            //then you can write code to list all the queue names          
-            foreach (Azure.Page<QueueItem> queuePage in myqueues)
-            {
-                foreach (QueueItem queue in queuePage.Values)
-                {
-                    if (queue.Name.StartsWith("flow"))
-                    {
-                        QserviceClient.DeleteQueue(queue.Name);
-                        Console.WriteLine($"-Queue {queue.Name} is Deleted ");
-                    }
-
-
-                }
-
-            }
-
-            Console.WriteLine("Done");
+            QserviceClient.DeleteQueue(queue.Name);
+            Console.WriteLine($"-Queue {queue.Name} is Deleted ");
         }
 
-        static private bool PromptConfirmation(string confirmText)
-        {
-            Console.Write(confirmText + " [y/n] : ");
-            string response = Console.ReadLine();
-            Console.WriteLine();
-            return (response.ToLower() =="y" );
-        }
 
     }
+
+}
+
+Console.WriteLine("Done");
+return;
+
+
+static bool PromptConfirmation(string confirmText)
+{
+    Console.Write(confirmText + " [y/n] : ");
+    var response = Console.ReadLine();
+    Console.WriteLine();
+    return (response?.ToLower() =="y" );
 }
